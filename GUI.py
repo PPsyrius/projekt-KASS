4127e75b5a3641839d31f1c49121eda8dcc40b73
PPsyrius <PPsyrius@users.noreply.github.com>
2020-05-31 04:04:55 +0700

    log-in system finished

--- GUI.py
+++ GUI.py
@@ -7,20 +7,15 @@ from PySide2.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QTable
 from PySide2.QtCore import QFile, QTimer, QAbstractTableModel, Qt
 from PySide2.QtUiTools import QUiLoader
 
-
-username_read = "Guest"
-header = ['Period', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-courseTableList = [
-    ['9:00-12:00','CMon','CTue','CWed','CThur','CFri']
-]
-
+from Global_var import *
+from sqliteDB import *
 
 class UI_form_login(QWidget):
     def __init__(self):
         super(UI_form_login, self).__init__()
         self.load_ui()
 
-        self.setWindowTitle('KMITL Academic Scheduler System: Authenthication')
+        self.setWindowTitle('KMITL Academic Scheduler System: Authentication')
         
         self.lb_logInStatus = self.findChild(QLabel, 'lb_logInStatus')
         self.bt_logIn = self.findChild(QPushButton, 'bt_logIn')
@@ -43,13 +38,26 @@ class UI_form_login(QWidget):
         self.lb_logInStatus.setText(text)
 
     def authenLogIn(self):
-        print(self.le_email.text())
-        print(self.le_password.text())
-        self.updateStatusMessage("Status: Login Attempted")
-        pass
+        email_inp = self.le_email.text()
+        password_inp = self.le_password.text()
+
+        login_q = session.query(Professor).filter_by(email=email_inp).first()
+        if not login_q:
+            self.updateStatusMessage("Status: Mismatched User") # Email not exist
+        elif login_q.password == password_inp:
+            self.updateStatusMessage("Status: Login Succesful") # Success
+            gvar.username_read = login_q.fullname
+            widget_menu.show()
+            widget_login.hide()
+        else:
+            self.updateStatusMessage("Status: Mismatched User") # Other Errors
+
+            print(password_inp)
+            print(login_q.password)
 
     def guestLogIn(self):
-        widget_menu.show()
+        gvar.username_read = "Guest"
+        widget_menu_guest.show()
         widget_login.hide()    
 
 class UI_form_main(QWidget):
@@ -57,7 +65,7 @@ class UI_form_main(QWidget):
         super(UI_form_main, self).__init__()
         self.load_ui()
 
-        self.setWindowTitle('KMITL Academic Scheduler System: ' + username_read)
+        self.setWindowTitle('KMITL Academic Scheduler System: ' + gvar.username_read)
 
         self.lb_welcome = self.findChild(QLabel, 'lb_welcome')
         self.lb_currentDateTime = self.findChild(QLabel, 'lb_currentDateTime')
@@ -68,7 +76,7 @@ class UI_form_main(QWidget):
         self.bt_logOut = self.findChild(QPushButton, 'bt_logOut')
         self.table_wholeSchedule = self.findChild(QTableView, 'table_wholeSchedule')
 
-        self.lb_welcome.setText("Welcome, " + username_read)
+        self.lb_welcome.setText("Welcome, " + gvar.username_read)
         self.lb_currentDateTime.setText(datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S'))
         self.bt_addCourse.clicked.connect(self.addCourse)
         self.bt_removeCourse.clicked.connect(self.removeCourse)
@@ -100,7 +108,7 @@ class UI_form_main(QWidget):
         pass
 
     def updateTable(self):
-        table_model = MyTableModel(self, courseTableList, header)
+        table_model = MyTableModel(self, courseTableList, gvar.header)
         self.table_wholeSchedule.setModel(table_model)
 
     def generateTable(self):
@@ -111,16 +119,64 @@ class UI_form_main(QWidget):
         pass
 
     def logOut(self):
-        username_read = "Guest"
+        gvar.username_read = "Guest"
         widget_login.show()
         widget_menu.hide()
 
 
+class UI_form_main_guest(QWidget):
+    def __init__(self):
+        super(UI_form_main_guest, self).__init__()
+        self.load_ui()
+
+        self.setWindowTitle('KMITL Academic Scheduler System: Guess View')
+
+        self.lb_welcome = self.findChild(QLabel, 'lb_welcome')
+        self.lb_currentDateTime = self.findChild(QLabel, 'lb_currentDateTime')
+        self.bt_exportPDF = self.findChild(QPushButton, 'bt_exportPDF')
+        self.bt_logOut = self.findChild(QPushButton, 'bt_logOut')
+        self.table_wholeSchedule = self.findChild(QTableView, 'table_wholeSchedule')
+
+        self.lb_welcome.setText("Welcome, " + gvar.username_read)
+        self.lb_currentDateTime.setText(datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S'))
+        self.bt_exportPDF.clicked.connect(self.exportPDF)
+        self.bt_logOut.clicked.connect(self.logOut)
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.updateCurrentTime)
+        timer.start(1000)
+
+        self.updateTable()
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.path.join(os.path.dirname(__file__), "form_main_guest.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        loader.load(ui_file, self)
+        ui_file.close()
+
+    def updateCurrentTime(self):
+        self.lb_currentDateTime.setText(datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S'))
+
+    def updateTable(self):
+        table_model = MyTableModel(self, courseTableList, gvar.header)
+        self.table_wholeSchedule.setModel(table_model)
+
+    def exportPDF(self):
+        pass
+
+    def logOut(self):
+        gvar.username_read = "Guest"
+        widget_login.show()
+        widget_menu_guest.hide()
+
+
 class MyTableModel(QAbstractTableModel):
-    def __init__(self, parent, mylist, header, *args):
+    def __init__(self, parent, mylist, gvar.header, *args):
         QAbstractTableModel.__init__(self, parent, *args)
         self.mylist = mylist
-        self.header = header
+        self.header = gvar.header
 
     def rowCount(self, parent):
         return len(self.mylist)
@@ -140,10 +196,12 @@ class MyTableModel(QAbstractTableModel):
             return self.header[col]
         return None
 
-
 if __name__ == "__main__":
     app = QApplication([])
+
     widget_menu = UI_form_main()
+    widget_menu_guest = UI_form_main_guest()
     widget_login = UI_form_login()
+
     widget_login.show()
     sys.exit(app.exec_())
---
+++ Global_var.py
@@ -0,0 +1,8 @@
+# This Python file uses the following encoding: utf-8
+# Global Variables are stored here
+
+username_read = "Guest"
+header = ['Period', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+courseTableList = [
+    ['9:00-12:00','CMon','CTue','CWed','CThur','CFri']
+]
