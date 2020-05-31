cfc4e6f4c12da898e478088a6c6f8c412fe1a735
PPsyrius <PPsyrius@users.noreply.github.com>
2020-05-31 09:12:04 +0700

    Remove mockup data

--- gvar.py
+++ gvar.py
@@ -4,11 +4,11 @@
 username_read = "Guest"
 scheduleHeader = ['Period', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 scheduleTableList = [
-    ['9:00-12:00','CMon','CTue','CWed','CThur','CFri']
+    []
 ]
 courseHeader = ['Course ID', 'Course Name', 'Class Capacity', 'Date', 'Period', 'Professor']
 courseTableList = [
-    ["A","B","C","D","E","F"]
+    []
 ]
 date = ["Mon", "Tue", "Wed", "Thu", "Fri"]
 time = ["09:00-12:00", "13:00-16:00"]
