5c654695f91d079942925d46577a57aae6fc17be
Aliapoh9 <anu5200@hotmail.com>
2020-06-01 20:57:19 +0700

    new database schema

--- sqliteDB.py
+++ sqliteDB.py
@@ -9,29 +9,73 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
+class Admin(Base):
+    __tablename__ = "admins"
+
+    AdminID = Column(String, primary_key=True)
+    AdminName = Column(String)
+    Email = Column(String)
+    Password = Column(String)
+
+    def __repr__(self):
+        return "Professor(AdminID = {}, AdminName = {}, Email = {}, Password = {})".format(self.AdminID, self.AdminName, self.Email, self.Password)
+
+
 class Professor(Base):
     __tablename__ = "professors"
 
-    profid = Column(String, primary_key=True)
-    fullname = Column(String)
-    email = Column(String)
-    password = Column(String)
+    ProfID = Column(String, primary_key=True)
+    ProfName = Column(String)
+    Email = Column(String)
+    Password = Column(String)
 
     def __repr__(self):
-        return "Professor(profid = {}, fullname = {}, email = {}, password = {})".format(self.profid, self.fullname, self.email, self.password)
+        return "Professor(ProfID = {}, ProfName = {}, Email = {}, Password = {})".format(self.ProfID, self.ProfName, self.Email, self.Password)
+
+
+class Room(Base):
+    __tablename__ = "rooms"
+
+    RoomID = Column(String, primary_key=True)
+    RoomType = Column(String) # ComLab, Lecture
+    Capacity = Column(Integer)
+
+    def __repr__(self):
+        return "Room(RoomID = {}, RoomType = {}, Capacity = {})".format(self.RoomID, self.RoomType, self.Capacity)
+
+
+class RoomOccupancy(Base):
+    __tablename__ = "roomOccupancies"
+
+    RoomID = Column(String, primary_key=True)
+    DateTime = Column(Integer)
+    CourseID = Column(String)
+
+    def __repr__(self):
+        return "RoomOccupancy(RoomID = {}, DateTime = {}, CourseID = {})".format(self.RoomID, self.DateTime, self.CourseID)
 
 
 class Course(Base):
     __tablename__ = 'courses'
 
-    courseid = Column(String, primary_key=True)
-    name = Column(String)
-    no_students = Column(Integer)
-    date = Column(Integer)  # Monday = 1 ... Friday = 5
-    time = Column(Integer)  # 1 for Morning, 2 for Afternoon
-    profname = Column(String)
+    CourseID = Column(String, primary_key=True)
+    CourseName = Column(String)
+    NoStudents = Column(Integer)
+    ProfName = Column(String)
+    RoomType = Column(String) # ComLab, Lecture
 
     def __repr__(self):
-        return "Course(courseid = {}, name = {}, no_students = {}, date = {}, time = {}, profname = {})".format(self.courseid, self.name, self.no_students, self.date, self.time, self.profname)
+        return "Course(CourseID = {}, CourseName = {}, NoStudents = {}, ProfName = {})".format(self.CourseID, self.CourseName, self.NoStudents, self.ProfName)
+
+
+class CourseTimeSlot(Base):
+    __tablename__ = "coursetimeslots"
+
+    CourseID = Column(String)
+    DateTime = Column(Integer)
+
+    def __repr__(self):
+        return "CourseTimeSlot(CourseID = {}, DateTime = {})".format(self.CourseID, self.DateTime)
+
 
 Base.metadata.create_all(engine)
