70c44d7fdf3e6b6e016043c2231f75a589a4a199
PPsyrius <PPsyrius@users.noreply.github.com>
2020-06-03 05:01:43 +0700

    Fix UI Shit, more UI shit added

--- GUI.py
+++ GUI.py
@@ -7,7 +7,10 @@ from PySide2.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QTable
 from PySide2.QtCore import QFile, QTimer, QAbstractTableModel, Qt
 from PySide2.QtUiTools import QUiLoader
 
-from algo import *
+from gvar import *
+
+global username_read
+username_read = "Dr Visit"
 
 class UI_form_login(QWidget):
     def __init__(self):
@@ -39,24 +42,245 @@ class UI_form_login(QWidget):
     def authenLogIn(self):
         email_inp = self.le_email.text()
         password_inp = self.le_password.text()
+        global username_read
 
-        login_q = session.query(Professor).filter_by(email=email_inp).first()
-        if not login_q:
-            self.updateStatusMessage("Status: Mismatched User") # Email not exist
-        elif login_q.password == password_inp:
-            self.updateStatusMessage("Status: Login Succesful") # Success
-            global username_read
-            username_read = login_q.fullname
+        login_p = session.query(Professor).filter_by(Email=email_inp).first()
+        login_a = session.query(Admin).filter_by(Email=email_inp).first()
+
+        if not (login_p or login_a):
+            self.updateStatusMessage("Status: Invalid User") # Email not exist
+        elif login_a.password == password_inp and login_a.Email == email_inp:
+            self.updateStatusMessage("Status: Login Succesful") # Success (Admin)
+            username_read = login_a.ProfName
             widget_menu.show()
+            widget_login.hide()
+        elif login_p.password == password_inp and login_p.Email == email_inp:
+            self.updateStatusMessage("Status: Login Succesful") # Success (Professor)
+            username_read = login_p.ProfName
+            widget_menu_prof.show()
             widget_login.hide()
         else:
             self.updateStatusMessage("Status: Mismatched User") # Other Errors
             print(password_inp)
+            print(login_a.password)
             print(login_q.password)
 
     def guestLogIn(self):
         widget_menu_guest.show()
         widget_login.hide()    
+
+
+class UI_form_pick_timeslot(QWidget):
+    def __init__(self):
+        super(UI_form_pick_timeslot, self).__init__()
+        self.load_ui()
+
+        self.setWindowTitle('KMITL Academic Scheduler System: Pick Timeslot')
+
+        self.cb_currentSubject = self.findChild(QComboBox, 'cb_currentSubject')
+        self.bt_11 = self.findChild(QPushButton, 'bt_11')
+        self.bt_21 = self.findChild(QPushButton, 'bt_21')
+        self.bt_31 = self.findChild(QPushButton, 'bt_31')
+        self.bt_41 = self.findChild(QPushButton, 'bt_41')
+        self.bt_51 = self.findChild(QPushButton, 'bt_51')
+        self.bt_12 = self.findChild(QPushButton, 'bt_12')
+        self.bt_22 = self.findChild(QPushButton, 'bt_22')
+        self.bt_32 = self.findChild(QPushButton, 'bt_32')
+        self.bt_42 = self.findChild(QPushButton, 'bt_42')
+        self.bt_52 = self.findChild(QPushButton, 'bt_52')
+        self.bt_back = self.findChild(QPushButton, 'bt_back')
+        self.lbb_ProfName = self.findChild(QLabel, 'lbb_ProfName')
+        self.lbb_NoStudents = self.findChild(QLabel, 'lbb_NoStudents')
+        self.lbb_RoomType = self.findChild(QLabel, 'lbb_RoomType')
+        self.lbb_CourseName = self.findChild(QLabel, 'lbb_CourseName')
+
+        self.updateSelectedCourseList()
+    
+        self.cb_currentSubject.addItems(selectedCourseList)
+        self.cb_currentSubject.currentTextChanged.connect(self.CbLoadState)
+        self.bt_11.clicked.connect(self.BtToggleState)
+        self.bt_21.clicked.connect(self.BtToggleState)
+        self.bt_31.clicked.connect(self.BtToggleState)
+        self.bt_41.clicked.connect(self.BtToggleState)
+        self.bt_51.clicked.connect(self.BtToggleState)
+        self.bt_12.clicked.connect(self.BtToggleState)
+        self.bt_22.clicked.connect(self.BtToggleState)
+        self.bt_32.clicked.connect(self.BtToggleState)
+        self.bt_42.clicked.connect(self.BtToggleState)
+        self.bt_52.clicked.connect(self.BtToggleState)
+        self.bt_back.clicked.connect(self.previousPage)
+
+        self.bt_11.setCheckable(True)
+        self.bt_21.setCheckable(True)
+        self.bt_31.setCheckable(True)
+        self.bt_41.setCheckable(True)
+        self.bt_51.setCheckable(True)
+        self.bt_12.setCheckable(True)
+        self.bt_22.setCheckable(True)
+        self.bt_32.setCheckable(True)
+        self.bt_42.setCheckable(True)
+        self.bt_52.setCheckable(True)
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.path.join(os.path.dirname(__file__), "form_pick_timeslot.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        loader.load(ui_file, self)
+        ui_file.close()
+
+    def updateSelectedCourseList(self):
+        global selectedCourseList
+        selectedCourseList = ["--Select Here--"]
+        for c in session.query(Course).filter_by(ProfName=username_read).order_by(Course.CourseID):
+            selectedCourseList.append(c.CourseID)
+        
+    def updateDB(self):
+        self.lbb_ProfName.setText('-')
+        self.lbb_NoStudents.setText('-')
+        self.lbb_RoomType.setText('-')
+        self.lbb_CourseName.setText('-')
+
+        cID = self.cb_currentSubject.currentText()
+        if cID!= "--Select Here--":
+            c = session.query(Course).filter_by(CourseID=cID).first()
+            self.lbb_ProfName.setText(c.ProfName)
+            self.lbb_NoStudents.setText(str(c.NoStudents))
+            self.lbb_RoomType.setText(c.RoomType)
+            self.lbb_CourseName.setText(c.CourseName)
+            
+        for c in session.query(Course).filter_by(Course.CourseID):
+            c_done = [c.CourseID,c.CourseName,c.NoStudents,c.ProfName,c.RoomType]
+            selectedCourseTableList.append(c_done)
+            selectedCourseList.append(c.CourseID)
+
+    def CbLoadState(self):
+        self.updateDB()
+
+        self.bt_11.setChecked(False)
+        self.bt_21.setChecked(False)
+        self.bt_31.setChecked(False)
+        self.bt_41.setChecked(False)
+        self.bt_51.setChecked(False)
+        self.bt_12.setChecked(False)
+        self.bt_22.setChecked(False)
+        self.bt_32.setChecked(False)
+        self.bt_42.setChecked(False)
+        self.bt_52.setChecked(False)
+        
+        cID = self.cb_currentSubject.currentText()
+        if cID!= "--Select Here--":
+            for ts in session.query(CourseTimeSlot).filter_by(CourseID=cID).order_by(CourseTimeSlot.DateTime):
+                if ts.DateTime==11:
+                    self.bt_11.setChecked(True)
+                elif ts.DateTime==21:
+                    self.bt_21.setChecked(True)
+                elif ts.DateTime==31:
+                    self.bt_31.setChecked(True)
+                elif ts.DateTime==41:
+                    self.bt_41.setChecked(True)
+                elif ts.DateTime==51:
+                    self.bt_51.setChecked(True)
+                elif ts.DateTime==12:
+                    self.bt_12.setChecked(True)
+                elif ts.DateTime==22:
+                    self.bt_22.setChecked(True)
+                elif ts.DateTime==32:
+                    self.bt_32.setChecked(True)
+                elif ts.DateTime==42:
+                    self.bt_42.setChecked(True)
+                elif ts.DateTime==52:
+                    self.bt_52.setChecked(True)
+                    
+    def BtToggleState(self):
+        cID = self.cb_currentSubject.currentText()
+        if cID!= "--Select Here--":
+            sender = self.sender()
+            print(self.sender().text())
+
+            ## Toggle Status ##
+            if sender.isChecked():
+                sender.setChecked(True)     # Select Timeslot
+            else:
+                sender.setChecked(False)    # Deselect Timeslot
+
+            if sender == self.bt_11:
+                if sender.isChecked():
+                    ctss = CourseTimeSlot(CourseID=cID, DateTime=11)
+                    session.add(ctss)
+                else:
+                    ctss = session.query(CourseTimeSlot).filter_by(CourseID=cID).filter_by(DateTime=11).first()
+                    session.delete(ctss)
+            elif sender == self.bt_21:
+                if sender.isChecked():
+                    ctss = CourseTimeSlot(CourseID=cID, DateTime=21)
+                    session.add(ctss)
+                else:
+                    ctss = session.query(CourseTimeSlot).filter_by(CourseID=cID).filter_by(DateTime=21).first()
+                    session.delete(ctss)
+            elif sender == self.bt_31:
+                if sender.isChecked():
+                    ctss = CourseTimeSlot(CourseID=cID, DateTime=31)
+                    session.add(ctss)
+                else:
+                    ctss = session.query(CourseTimeSlot).filter_by(CourseID=cID).filter_by(DateTime=31).first()
+                    session.delete(ctss)
+            elif sender == self.bt_41:
+                if sender.isChecked():
+                    ctss = CourseTimeSlot(CourseID=cID, DateTime=41)
+                    session.add(ctss)
+                else:
+                    ctss = session.query(CourseTimeSlot).filter_by(CourseID=cID).filter_by(DateTime=41).first()
+                    session.delete(ctss)
+            elif sender == self.bt_51:
+                if sender.isChecked():
+                    ctss = CourseTimeSlot(CourseID=cID, DateTime=51)
+                    session.add(ctss)
+                else:
+                    ctss = session.query(CourseTimeSlot).filter_by(CourseID=cID).filter_by(DateTime=51).first()
+                    session.delete(ctss)
+            elif sender == self.bt_12:
+                if sender.isChecked():
+                    ctss = CourseTimeSlot(CourseID=cID, DateTime=12)
+                    session.add(ctss)
+                else:
+                    ctss = session.query(CourseTimeSlot).filter_by(CourseID=cID).filter_by(DateTime=12).first()
+                    session.delete(ctss)
+            elif sender == self.bt_22:
+                if sender.isChecked():
+                    ctss = CourseTimeSlot(CourseID=cID, DateTime=22)
+                    session.add(ctss)
+                else:
+                    ctss = session.query(CourseTimeSlot).filter_by(CourseID=cID).filter_by(DateTime=22).first()
+                    session.delete(ctss)
+            elif sender == self.bt_32:
+                if sender.isChecked():
+                    ctss = CourseTimeSlot(CourseID=cID, DateTime=32)
+                    session.add(ctss)
+                else:
+                    ctss = session.query(CourseTimeSlot).filter_by(CourseID=cID).filter_by(DateTime=32).first()
+                    session.delete(ctss)
+            elif sender == self.bt_42:
+                if sender.isChecked():
+                    ctss = CourseTimeSlot(CourseID=cID, DateTime=42)
+                    session.add(ctss)
+                else:
+                    ctss = session.query(CourseTimeSlot).filter_by(CourseID=cID).filter_by(DateTime=42).first()
+                    session.delete(ctss)
+            elif sender == self.bt_52:
+                if sender.isChecked():
+                    ctss = CourseTimeSlot(CourseID=cID, DateTime=52)
+                    session.add(ctss)
+                else:
+                    ctss = session.query(CourseTimeSlot).filter_by(CourseID=cID).filter_by(DateTime=52).first()
+                    session.delete(ctss)
+
+            ##REMOVE_THIS##session.commit()
+
+    def previousPage(self):
+        #self.updateTable()
+        pass
+
 
 class UI_form_main(QWidget):
     def __init__(self):
@@ -184,6 +408,7 @@ class UI_form_main_guest(QWidget):
         widget_login.show()
         widget_menu_guest.hide()
 
+
 class UI_course_remove(QWidget):
     def __init__(self):
         super(UI_course_remove, self).__init__()
@@ -213,13 +438,17 @@ class UI_course_remove(QWidget):
     def updateStatusMessage(self, text):
         self.lb_statusMessage.setText(text)
 
+    def updateCourseList(self):
+        global courseList
+        courseList = []
+        for c in session.query(Course).order_by(Course.CourseID):
+            profList.append(c.CourseID)
+
     def updateDB(self):
         global courseTableList
         courseTableList = []
-        for c in session.query(Course).order_by(Course.courseid).filter_by(profname=username_read):
-            c_done = []
-            c_done.extend([c.courseid,c.name,c.no_students])
-            c_done.extend([date[c.date-1],time[c.time-1],c.profname])
+        for c in session.query(Course).order_by(Course.CourseID):
+            c_done = [c.CourseID,c.CourseName,c.NoStudents,c.ProfName,c.RoomType]
             courseTableList.append(c_done)
 
     def updateTable(self):
@@ -232,7 +461,7 @@ class UI_course_remove(QWidget):
     def removeCourse(self):
         courseID_inp = self.le_courseID.text()
 
-        cd = session.query(Course).filter_by(courseid=courseID_inp).first()
+        cd = session.query(Course).filter_by(CourseID=courseID_inp).first()
         
         if not courseTableList:
             self.updateStatusMessage("Error: No Course in DB by this User")
@@ -244,7 +473,8 @@ class UI_course_remove(QWidget):
             self.updateStatusMessage("Status: Course Removed") # Success
             session.delete(cd)
             
-        self.updateTable()            
+        self.updateTable()
+        self.updateCourseList()
         ##REMOVE_THIS##session.commit()
 
     def previousPage(self):
@@ -252,6 +482,7 @@ class UI_course_remove(QWidget):
         widget_course_remove.hide()
         widget_course_add.updateStatusMessage("Click Remove Once To Pull Data")
         widget_course_remove.updateStatusMessage("Click Remove Once To Pull Data")
+
 
 class UI_course_add(QWidget):
     def __init__(self):
@@ -263,15 +494,15 @@ class UI_course_add(QWidget):
         self.le_courseID = self.findChild(QLineEdit, 'le_courseID')
         self.le_courseName = self.findChild(QLineEdit, 'le_courseName')
         self.le_capacity = self.findChild(QLineEdit, 'le_capacity')
-        self.cb_date = self.findChild(QComboBox, 'cb_date')
-        self.cb_time = self.findChild(QComboBox, 'cb_time')
+        self.cb_prof = self.findChild(QComboBox, 'cb_prof')
+        self.cb_roomtype = self.findChild(QComboBox, 'cb_roomtype')
         self.lb_statusMessage = self.findChild(QLabel, 'lb_statusMessage')
         self.bt_back = self.findChild(QPushButton, 'bt_back')
         self.bt_add = self.findChild(QPushButton, 'bt_add')
         self.table_courseList = self.findChild(QTableView, 'table_courseList')
 
-        self.cb_date.addItems(date)
-        self.cb_time.addItems(time)
+        self.cb_prof.addItems(profList)
+        self.cb_roomtype.addItems(roomType)
         self.bt_back.clicked.connect(self.previousPage)
         self.bt_add.clicked.connect(self.addCourse)
 
@@ -288,13 +519,18 @@ class UI_course_add(QWidget):
     def updateStatusMessage(self, text):
         self.lb_statusMessage.setText(text)
 
+
+    def updateCourseList(self):
+        global courseList
+        courseList = []
+        for c in session.query(Course).order_by(Course.CourseID):
+            profList.append(c.CourseID)
+
     def updateDB(self):
         global courseTableList
         courseTableList = []
-        for c in session.query(Course).order_by(Course.courseid).filter_by(profname=username_read):
-            c_done = []
-            c_done.extend([c.courseid,c.name,c.no_students])
-            c_done.extend([date[c.date-1],time[c.time-1],c.profname])
+        for c in session.query(Course).order_by(Course.CourseID):
+            c_done = [c.CourseID,c.CourseName,c.NoStudents,c.ProfName,c.RoomType]
             courseTableList.append(c_done)
 
     def updateTable(self):
@@ -308,17 +544,14 @@ class UI_course_add(QWidget):
         courseID_inp = self.le_courseID.text()
         courseName_inp = self.le_courseName.text()
         capacity_inp = self.le_capacity.text()
-        date_inp = self.cb_date.currentIndex()+1
-        time_inp = self.cb_time.currentIndex()+1
-
-        cd = session.query(Course).filter_by(courseid=courseID_inp).first()
-        cd2 = session.query(Course).filter_by(time=time_inp).filter_by(date=date_inp).filter_by(profname=username_read).first()
+        prof_inp = self.cb_prof.currentText()
+        roomtype_inp = self.cb_roomtype.currentText()
         
         if not courseTableList:
             self.updateStatusMessage("Error: No Course in DB by this User")
         elif courseID_inp=="":
             self.updateStatusMessage("Status: Table Updated") # Pull Data
-        elif cd:
+        elif courseID_inp in courseList:
             self.updateStatusMessage("Error: Course ID Exists") # CourseID exists
         elif courseName_inp=="" or capacity_inp=="":
             self.updateStatusMessage("Error: Blank Data Slots") # Blank slots
@@ -326,13 +559,12 @@ class UI_course_add(QWidget):
             self.updateStatusMessage("Error: Capacity must be Integer") # Capacity Not Integer
         elif int(capacity_inp)<=0:
             self.updateStatusMessage("Error: Capacity must be Positive") # <=0
-        elif cd2:
-            self.updateStatusMessage("Error: Timeslot Used") # Invalid Timeslot
         else:
             self.updateStatusMessage("Status: Course Added") # Success
-            ca = Course(courseid=courseID_inp, name=courseName_inp, no_students=int(capacity_inp), date=date_inp, time=time_inp, profname=username_read)
+            ca = Course(CourseID=courseID_inp, CourseName=courseName_inp, NoStudents=int(capacity_inp), ProfName=prof_inp, RoomType=roomtype_inp)
             session.add(ca)                     
-        self.updateTable()            
+        self.updateTable()
+        self.updateCourseList()
         ##REMOVE_THIS##session.commit()
 
     def previousPage(self):
@@ -340,6 +572,7 @@ class UI_course_add(QWidget):
         widget_course_add.hide()
         widget_course_add.updateStatusMessage("Click Remove Once To Pull Data")
         widget_course_remove.updateStatusMessage("Click Remove Once To Pull Data")
+
 
 class MyTableModel(QAbstractTableModel):
     def __init__(self, parent, mylist, header, *args):
@@ -368,21 +601,15 @@ class MyTableModel(QAbstractTableModel):
 if __name__ == "__main__":
     app = QApplication([])
 
-    widget_menu = UI_form_main()
-    widget_menu_guest = UI_form_main_guest()
     widget_login = UI_form_login()
+    widget_pick_timeslot = UI_form_pick_timeslot()
     widget_course_remove = UI_course_remove()
     widget_course_add = UI_course_add()
+    #widget_menu = UI_form_main()
+    #widget_menu_guest = UI_form_main_guest()
 
     widget_login.show()
-    sys.exit(app.exec_())
 
-'''      
-for row in range([index.row()]):
-    item[0] = new QTableWidgetItem(QString(text).arg(row+1))
-    item[1] = new QTableWidgetItem(QString(text).arg(row+1))
-    item[2] = new QTableWidgetItem(QString(text).arg(row+1))
- 
-    for col in range([index.col()]):
-        tableWidget.setItem(row, col, item[col])
-'''
+    #widget_menu.show()
+    
+    sys.exit(app.exec_())
--- gvar.py
+++ gvar.py
@@ -1,18 +1,27 @@
 # This Python file uses the following encoding: utf-8
 # Global Variables are stored here
 
+from sqliteDB import *
+
 username_read = "Guest"
-#scheduleHeader = ['Period', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-scheduleHeader = ['Date', 'Time', 'ClassID', 'Class', 'Lecturer', 'Location']
+scheduleHeader = [ 'Location', 'Date', 'Time', 'ClassID', 'Class', 'Lecturer']
 scheduleTableList = [
-    ["","","","","",""]
+    ['','','','','','']
 ]
-courseHeader = ['Course ID', 'Course Name', 'Class Capacity', 'Date', 'Period', 'Professor']
+courseHeader = ['Course ID', 'Course Name', 'Class Capacity', 'Lecturer', 'Classroom Type']
 courseTableList = [
-    []
+    ['','','','','']
 ]
 date = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-time = ["09:00-12:00", "13:00-16:00"]
-rooms = 5
+time = ['09:00-12:00', '13:00-16:00']
+roomType = ['Lecture', 'ComLab']
 
+profList = []
+for p in session.query(Professor).order_by(Professor.ProfName):
+    profList.append(p.ProfName)
 
+courseList = []
+for c in session.query(Course).order_by(Course.CourseID):
+    profList.append(c.CourseID)
+
+selectedCourseList = ["--Select Here--"]
